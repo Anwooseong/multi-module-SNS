e875e2543a7174f0d69dbcd056952acbc279227f
Anwooseong <aws5624@naver.com>
2025-06-13 19:12:08 +0900

    refactor: @EnableJpaAuditing 코드 추가

--- api/src/main/java/com/social/config/jpa/JpaConfig.java
+++ api/src/main/java/com/social/config/jpa/JpaConfig.java
@@ -2,10 +2,12 @@ package com.social.config.jpa;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
-@EntityScan(basePackages = "com.social.domain")
-@EnableJpaRepositories(basePackages = "com.social.domain")
+@EnableJpaAuditing
+@EntityScan(basePackages = "com.social")
+@EnableJpaRepositories(basePackages = "com.social")
 public class JpaConfig {
 }
