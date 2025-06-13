530c500172aba2c0d46e52ea05735443f6df3462
Anwooseong <aws5624@naver.com>
2025-06-13 18:05:35 +0900

    chore: jwt gradle 추가

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -3,4 +3,8 @@ dependencies {
     //core
     implementation(project("path" to ":core"))
     implementation("org.springframework.boot:spring-boot-starter-web")
+
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
 }
