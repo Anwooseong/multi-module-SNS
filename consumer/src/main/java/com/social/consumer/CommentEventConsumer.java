50de94800b2337f60613f9712e8604e0f06e5ce8
Anwooseong <aws5624@naver.com>
2025-06-20 17:51:22 +0900

    style: 필요없는 주석 제거

--- consumer/src/main/java/com/social/consumer/CommentEventConsumer.java
+++ consumer/src/main/java/com/social/consumer/CommentEventConsumer.java
@@ -23,7 +23,7 @@ public class CommentEventConsumer {
     public Consumer<CommentEvent> comment() {  //application-event.yml에 등록된 function definition에 따라서 함수명 설정
         return event -> {
             if (event.getType() == CommentEventType.ADD) {
-                commentAddTask.processEvent(event); // <-- 이 메서드 내부에서 오류 발생 가능성 높음
+                commentAddTask.processEvent(event);
             } else if (event.getType() == CommentEventType.REMOVE) {
                 commentRemoveTask.processEvent(event);
             }
