c00b3d6afb5440b42e5d4738b6086ea4b125334e
Anwooseong <aws5624@naver.com>
2025-07-07 18:36:04 +0900

    docs: 팔로우 취소 API Swagger 추가

--- api/src/main/java/com/social/controller/FollowsControllerSpec.java
+++ api/src/main/java/com/social/controller/FollowsControllerSpec.java
@@ -2,6 +2,7 @@ package com.social.controller;
 
 import com.social.controller.request.CreateFollowRequest;
 import com.social.controller.response.CreateFollowResponse;
+import com.social.controller.response.DeleteFollowResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
@@ -12,5 +13,10 @@ public interface FollowsControllerSpec {
     @Operation(summary = "팔로우 추가")
     ResponseEntity<CreateFollowResponse> createFollow(
             CreateFollowRequest createFollowRequest
+    );
+
+    @Operation(summary = "팔로우 취소")
+    ResponseEntity<DeleteFollowResponse> deleteFollow(
+            Long toUserId
     );
 }
