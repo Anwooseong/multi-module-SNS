96157711befda22732bceb7af36537d05d87d582
Anwooseong <aws5624@naver.com>
2025-07-28 18:17:29 +0900

    feat: 게시글 조회

--- api/src/main/java/com/social/controller/PostsController.java
+++ api/src/main/java/com/social/controller/PostsController.java
@@ -3,11 +3,15 @@ package com.social.controller;
 import com.social.controller.request.CreatePostsRequest;
 import com.social.controller.response.CreatePostsResponse;
 import com.social.controller.response.SliceResponse;
+import com.social.repository.querydslDTO.GetPostsDTO;
 import com.social.service.PostsService;
+import com.social.util.SecurityUtil;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -16,15 +20,16 @@ public class PostsController implements PostsControllerSpec{
 
     private final PostsService postsService;
 
+    @Override
     @PostMapping
     public ResponseEntity<CreatePostsResponse> createBoard(@Valid @RequestBody CreatePostsRequest createPostsRequest) {
         return ResponseEntity.ok(CreatePostsResponse.of(postsService.createPost(createPostsRequest)));
     }
 
+    @Override
     @GetMapping
-    public ResponseEntity<SliceResponse<?>> getFeed() {
-
-        return null;
+    public ResponseEntity<SliceResponse<GetPostsDTO>> getFeed(@RequestParam("page") int page, @RequestParam("size") int size) {
+        return ResponseEntity.ok(postsService.getFeed(SecurityUtil.getCurrentUserId(), PageRequest.of(page, size)));
     }
 
 }
--- api/src/main/java/com/social/controller/PostsControllerSpec.java
+++ api/src/main/java/com/social/controller/PostsControllerSpec.java
@@ -2,7 +2,10 @@ package com.social.controller;
 
 import com.social.controller.request.CreatePostsRequest;
 import com.social.controller.response.CreatePostsResponse;
+import com.social.controller.response.SliceResponse;
+import com.social.repository.querydslDTO.GetPostsDTO;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 
@@ -12,4 +15,9 @@ public interface PostsControllerSpec {
     @Operation(summary = "게시글 등록")
     ResponseEntity<CreatePostsResponse> createBoard(CreatePostsRequest createPostsRequest);
 
+    @Operation(summary = "게시글 조회")
+    ResponseEntity<SliceResponse<GetPostsDTO>> getFeed(
+            @Parameter(example = "0") int page,
+            @Parameter(example = "2") int size
+    );
 }
