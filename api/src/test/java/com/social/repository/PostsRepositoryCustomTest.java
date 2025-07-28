0720762204fc05db9ba76a8764c172da66554237
Anwooseong <aws5624@naver.com>
2025-07-28 16:26:33 +0900

    test: 게시글 조회 Querydsl repository 추가 테스트 코드 작성

--- api/src/test/java/com/social/repository/PostsRepositoryCustomTest.java
+++ api/src/test/java/com/social/repository/PostsRepositoryCustomTest.java
@@ -7,6 +7,7 @@ import com.social.repository.querydslDTO.GetPostsDTO;
 import com.social.util.*;
 import jakarta.persistence.EntityManager;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -45,6 +46,14 @@ class PostsRepositoryCustomTest {
     @Autowired
     private PhotosRepository photosRepository;
 
+    @AfterEach
+    void tearDown() {
+        likesRepository.deleteAllInBatch();
+        commentsRepository.deleteAllInBatch();
+        photosRepository.deleteAllInBatch();
+        postsRepository.deleteAllInBatch();
+        usersRepository.deleteAllInBatch();
+    }
 
     @DisplayName("게시글 조회 테스트")
     @Test
@@ -125,6 +134,57 @@ class PostsRepositoryCustomTest {
         Assertions.assertThat(dto.getLiked()).isFalse();
         Assertions.assertThat(dto.getCommentCnt()).isEqualTo(1L);
         Assertions.assertThat(dto.getThumbnailUrl()).isEqualTo("url");
+    }
+
+    @DisplayName("게시글 2번째 페이지 확인")
+    @Test
+    void testFindPostsSummaries_secondPage() {
+        // given
+        Users user1 = usersRepository.save(UserFixture.user("user1"));
+        Users user2 = usersRepository.save(UserFixture.user("user2"));
+
+        Posts post1 = postsRepository.save(PostFixture.post(user1, "test1"));
+        Posts post2 = postsRepository.save(PostFixture.post(user2, "test2"));
+        Posts post3 = postsRepository.save(PostFixture.post(user1, "test3"));
+        Posts post4 = postsRepository.save(PostFixture.post(user2, "test4"));
+        Posts post5 = postsRepository.save(PostFixture.post(user2, "test5"));
+
+        commentsRepository.save(CommentFixture.comment(user1, post1, "댓글1"));
+        commentsRepository.save(CommentFixture.comment(user1, post1, "댓글2"));
+
+        likesRepository.save(LikeFixture.like(user1, post1));
+        likesRepository.save(LikeFixture.like(user2, post1));
+        likesRepository.save(LikeFixture.like(user1, post2));
+        likesRepository.save(LikeFixture.like(user2, post4));
+
+        photosRepository.save(PhotoFixture.photo(post1, "url1", 1));
+        photosRepository.save(PhotoFixture.photo(post1, "url1", 2));
+        photosRepository.save(PhotoFixture.photo(post2, "url1", 1));
+        photosRepository.save(PhotoFixture.photo(post2, "url1", 2));
+        photosRepository.save(PhotoFixture.photo(post3, "url1", 1));
+        photosRepository.save(PhotoFixture.photo(post3, "url1", 2));
+        photosRepository.save(PhotoFixture.photo(post4, "url1", 1));
+
+        Pageable pageable = PageRequest.of(1, 2);
+
+        // when
+        Slice<GetPostsDTO> result = postsRepositoryCustomImpl.findPostsSummaries(user1.getId(), pageable);
+        System.out.println("총 개수: " + result.getContent().size());
+        result.getContent().forEach(dto -> {
+            System.out.println("------------");
+            System.out.println("postId: " + dto.getPostId());
+            System.out.println("caption: " + dto.getCaption());
+            System.out.println("liked: " + dto.getLiked());
+            System.out.println("commentCnt: " + dto.getCommentCnt());
+            System.out.println("createAt: " + dto.getCreateAt());
+            System.out.println("thumbnailUrl: " + dto.getThumbnailUrl());
+        });
+
+
+        // then
+        Assertions.assertThat(result.getContent().get(0).getPostId()).isEqualTo(post3.getId());
+        Assertions.assertThat(result.getContent()).hasSize(2);
+        Assertions.assertThat(result.hasNext()).isTrue();
     }
 
 }
