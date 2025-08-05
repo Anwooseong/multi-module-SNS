d4e22b23125b8a8d7377146d5b35b79d04cb3889
Anwooseong <aws5624@naver.com>
2025-08-05 17:27:16 +0900

    feat: 게시글 수정 컨트롤러

--- api/src/main/java/com/social/controller/PostsController.java
+++ api/src/main/java/com/social/controller/PostsController.java
@@ -1,8 +1,10 @@
 package com.social.controller;
 
 import com.social.controller.request.CreatePostsRequest;
+import com.social.controller.request.UpdatePostsRequest;
 import com.social.controller.response.CreatePostsResponse;
 import com.social.controller.response.SliceResponse;
+import com.social.controller.response.UpdatePostsResponse;
 import com.social.repository.querydslDTO.GetPostsDTO;
 import com.social.service.PostsService;
 import com.social.util.SecurityUtil;
@@ -11,6 +13,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import static com.social.util.SecurityUtil.getCurrentUserId;
 
 
 @RestController
@@ -31,5 +35,12 @@ public class PostsController implements PostsControllerSpec{
     public ResponseEntity<SliceResponse<GetPostsDTO>> getFeed(@RequestParam("page") int page, @RequestParam("size") int size) {
         return ResponseEntity.ok(postsService.getFeed(SecurityUtil.getCurrentUserId(), PageRequest.of(page, size)));
     }
+
+    @Override
+    @PutMapping("/{postId}")
+    public ResponseEntity<UpdatePostsResponse> updateBoard(@PathVariable("postId") Long postId, @Valid @RequestBody UpdatePostsRequest updatePostsRequest) {
+        return ResponseEntity.ok(UpdatePostsResponse.of(postsService.updatePost(getCurrentUserId(), postId, updatePostsRequest)));
+    }
+
 
 }
