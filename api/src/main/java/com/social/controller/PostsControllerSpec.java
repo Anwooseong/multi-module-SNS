133823ba36b24d7dbf3826924482c7bbd6acdfa5
Anwooseong <aws5624@naver.com>
2025-08-05 17:27:43 +0900

    docs: 게시글 수정 API Swagger 추가

--- api/src/main/java/com/social/controller/PostsControllerSpec.java
+++ api/src/main/java/com/social/controller/PostsControllerSpec.java
@@ -1,8 +1,10 @@
 package com.social.controller;
 
 import com.social.controller.request.CreatePostsRequest;
+import com.social.controller.request.UpdatePostsRequest;
 import com.social.controller.response.CreatePostsResponse;
 import com.social.controller.response.SliceResponse;
+import com.social.controller.response.UpdatePostsResponse;
 import com.social.repository.querydslDTO.GetPostsDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -20,4 +22,11 @@ public interface PostsControllerSpec {
             @Parameter(example = "0") int page,
             @Parameter(example = "2") int size
     );
+
+    @Operation(summary = "게시글 수정")
+    ResponseEntity<UpdatePostsResponse> updateBoard(
+            @Parameter(example = "1") Long postId,
+            UpdatePostsRequest updatePostsRequest
+    );
+
 }
