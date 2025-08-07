fa146eb7d9d1d6bf95907b7267a51410de287f15
Anwooseong <aws5624@naver.com>
2025-08-07 18:18:05 +0900

    docs: 게시글 댓글 수정 API Swagger 추가

--- api/src/main/java/com/social/controller/CommentsControllerSpec.java
+++ api/src/main/java/com/social/controller/CommentsControllerSpec.java
@@ -1,6 +1,6 @@
 package com.social.controller;
 
-import com.social.controller.request.CreateCommentsRequest;
+import com.social.controller.request.CommentRequest;
 import com.social.controller.response.CreateCommentsResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -13,6 +13,13 @@ public interface CommentsControllerSpec {
     @Operation(summary = "댓글 생성")
     ResponseEntity<CreateCommentsResponse> createComment(
             @Parameter(example = "1") Long poseId,
-            CreateCommentsRequest createCommentsRequest
+            CommentRequest commentRequest
+    );
+
+    @Operation(summary = "댓글 수정")
+    ResponseEntity<?> modifyComment(
+            @Parameter(example = "1") Long postId,
+            @Parameter(example = "1") Long commentId,
+            CommentRequest commentRequest
     );
 }
