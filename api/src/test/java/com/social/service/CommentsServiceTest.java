817bd0a37513da1126c216ab4277dac96839909f
Anwooseong <aws5624@naver.com>
2025-08-10 20:11:53 +0900

    test: 게시글 댓글 조회 서비스 테스트

--- api/src/test/java/com/social/service/CommentsServiceTest.java
+++ api/src/test/java/com/social/service/CommentsServiceTest.java
@@ -2,6 +2,7 @@ package com.social.service;
 
 import com.social.IntegrationTestSupport;
 import com.social.controller.request.CommentRequest;
+import com.social.controller.response.SliceResponse;
 import com.social.domain.Comments;
 import com.social.domain.Posts;
 import com.social.domain.Users;
@@ -9,15 +10,18 @@ import com.social.event.CommentEvent;
 import com.social.repository.CommentsRepository;
 import com.social.repository.PostsRepository;
 import com.social.repository.UsersRepository;
+import com.social.repository.querydslDTO.GetCommentDTO;
 import com.social.util.CommentFixture;
 import com.social.util.PostFixture;
 import com.social.util.UserFixture;
 import org.assertj.core.api.Assertions;
+import org.assertj.core.groups.Tuple;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.kafka.core.KafkaTemplate;
 
 import java.util.Optional;
@@ -142,4 +146,81 @@ class CommentsServiceTest extends IntegrationTestSupport {
                 .hasMessage("조건에 맞는 댓글이 존재하지 않습니다.");
     }
 
+    @DisplayName("게시글 댓글 조회 첫번째 페이지")
+    @Test
+    void getComments_firstPage() {
+        // given
+        Users user1 = usersRepository.save(UserFixture.user("user1"));
+        Users user2 = usersRepository.save(UserFixture.user("user2"));
+        Users user3 = usersRepository.save(UserFixture.user("user3"));
+
+        Posts post1 = postsRepository.save(PostFixture.post(user1, "test1"));
+        Posts post2 = postsRepository.save(PostFixture.post(user2, "test2"));
+
+        Comments comment1 = commentsRepository.save(CommentFixture.comment(user1, post1, "댓글1"));
+        Comments comment2 = commentsRepository.save(CommentFixture.comment(user2, post1, "댓글2"));
+        Comments comment3 = commentsRepository.save(CommentFixture.comment(user3, post1, "댓글3"));
+        Comments comment4 = commentsRepository.save(CommentFixture.comment(user3, post1, "댓글4"));
+        Comments comment5 = commentsRepository.save(CommentFixture.comment(user3, post1, "댓글5"));
+        Comments comment6 = commentsRepository.save(CommentFixture.comment(user3, post1, "댓글6"));
+
+        // when
+        SliceResponse<GetCommentDTO> result = commentsService.getComments(post1.getId(), PageRequest.of(0, 5));
+
+        // then
+        Assertions.assertThat(result.getCurrentPage()).isEqualTo(0);
+        Assertions.assertThat(result.getPageSize()).isEqualTo(5);
+        Assertions.assertThat(result.isHasNext()).isTrue();
+        Assertions.assertThat(result.getData()).hasSize(5);
+
+        Assertions.assertThat(result.getData())
+                .extracting("userId", "username", "commentId", "content")
+                .containsExactly(
+                        Tuple.tuple(user3.getId(), user3.getUsername(), comment6.getId(), "댓글6"),
+                        Tuple.tuple(user3.getId(), user3.getUsername(), comment5.getId(), "댓글5"),
+                        Tuple.tuple(user3.getId(), user3.getUsername(), comment4.getId(), "댓글4"),
+                        Tuple.tuple(user3.getId(), user3.getUsername(), comment3.getId(), "댓글3"),
+                        Tuple.tuple(user2.getId(), user2.getUsername(), comment2.getId(), "댓글2")
+                );
+
+        SliceResponse<GetCommentDTO> resultPost2 = commentsService.getComments(post2.getId(), PageRequest.of(0, 5));
+        Assertions.assertThat(resultPost2.getData()).isEmpty();
+    }
+
+    @DisplayName("게시글 댓글 조회 두번째 페이지")
+    @Test
+    void getComments_secondPage() {
+        // given
+        Users user1 = usersRepository.save(UserFixture.user("user1"));
+        Users user2 = usersRepository.save(UserFixture.user("user2"));
+        Users user3 = usersRepository.save(UserFixture.user("user3"));
+
+        Posts post1 = postsRepository.save(PostFixture.post(user1, "test1"));
+        Posts post2 = postsRepository.save(PostFixture.post(user2, "test2"));
+
+        Comments comment1 = commentsRepository.save(CommentFixture.comment(user1, post1, "댓글1"));
+        Comments comment2 = commentsRepository.save(CommentFixture.comment(user2, post1, "댓글2"));
+        Comments comment3 = commentsRepository.save(CommentFixture.comment(user3, post1, "댓글3"));
+        Comments comment4 = commentsRepository.save(CommentFixture.comment(user3, post1, "댓글4"));
+        Comments comment5 = commentsRepository.save(CommentFixture.comment(user3, post1, "댓글5"));
+        Comments comment6 = commentsRepository.save(CommentFixture.comment(user3, post1, "댓글6"));
+
+        // when
+        SliceResponse<GetCommentDTO> result = commentsService.getComments(post1.getId(), PageRequest.of(1, 5));
+
+        // then
+        Assertions.assertThat(result.getCurrentPage()).isEqualTo(1);
+        Assertions.assertThat(result.getPageSize()).isEqualTo(5);
+        Assertions.assertThat(result.isHasNext()).isFalse();
+        Assertions.assertThat(result.getData()).hasSize(1);
+
+        Assertions.assertThat(result.getData())
+                .extracting("userId", "username", "commentId", "content")
+                .containsExactly(
+                        Tuple.tuple(user1.getId(), user1.getUsername(), comment1.getId(), "댓글1")
+                );
+
+        SliceResponse<GetCommentDTO> resultPost2 = commentsService.getComments(post2.getId(), PageRequest.of(0, 5));
+        Assertions.assertThat(resultPost2.getData()).isEmpty();
+    }
 }
